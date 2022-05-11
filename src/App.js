74e90dd4d782563be91a72e6cf5fac2d84c54df7
Sinebi Innazo <sinebi.innazo@gmail.com>
2022-05-11 02:26:53 +0100

    add starter files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import NavBar from './components/layouts/NavBar';
+import './App.css';
+
+class App extends Component{
+  
+  render(){
+    return (
+
+      <div className='App'>
+        <NavBar />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
---
+++ src/components/layouts/NavBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NavBar extends Component {
+  render() {
+    return (
+      <nav className='navbar bg-primary'>
+        <h1>   <i class="fa-brands fa-github-square"></i> My Nav Bar</h1>
+      </nav>
+    )
+  }
+}
+
+export default NavBar
